c39647c77d32100220e40339f34bca4a497f036d
Valeria Garzón <valgarna@gmail.com>
2023-09-21 18:11:02 -0600

    Código corregido y validado por W3C

--- practicas/p05/get_productos_xhtml.php
+++ practicas/p05/get_productos_xhtml.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Productos</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <h3>PRODUCTOS</h3>
-    <br/>
+<?php
+    header("Content-Type: application/xhtml+xml; charset=utf-8");
 
-    <?php
+
+    $data = array();
+
     if(isset($_GET['tope']))
     {
         $tope = $_GET['tope'];
@@ -27,62 +20,74 @@
         /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 
         /** comprobar la conexión */
-        if ($link->connect_errno) 
+        if ($link->connect_errno)
         {
             die('Falló la conexión: '.$link->connect_error.'<br/>');
         }
 
         /** Crear una tabla que no devuelve un conjunto de resultados */
-        if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope"))
+        if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") )
         {
-            $data = $result->fetch_all(MYSQLI_ASSOC);
+            /** Se extraen las tuplas obtenidas de la consulta */
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+
+            /** Inicio del documento XHTML */
+            echo '<?xml version="1.0" encoding="UTF-8"?>';
+            echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">';
+            echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">';
+            echo '<head>';
+            echo '<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />';
+            echo '<title>Productos</title>';
+            echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>';
+            echo '</head>';
+            echo '<body>';
+            echo '<h3>PRODUCTOS</h3>' ;
+
+            /** Validación de W3C */
+            echo '<h3><p>
+            <a href="http://validator.w3.org/check?uri=referer"><img
+              src="http://www.w3.org/Icons/valid-xhtml11" alt="XHTML 1.1 válido" height="31" width="88" /></a>
+          </p></h3>' ;
+
+
+            echo '<table class="table">';
+            echo '<thead class="thead-dark">';
+            echo '<tr>';
+            echo '<th scope="col">#</th>';
+            echo '<th scope="col">Nombre</th>';
+            echo '<th scope="col">Marca</th>';
+            echo '<th scope="col">Modelo</th>';
+            echo '<th scope="col">Precio</th>';
+            echo '<th scope="col">Unidades</th>';
+            echo '<th scope="col">Detalles</th>';
+            echo '<th scope="col">Imagen</th>';
+            echo '</tr>';
+            echo '</thead>';
+            echo '<tbody>';
+
+         
+            foreach($row as $producto) {
+                echo '<tr>';
+                echo '<th scope="row">' . $producto['id'] . '</th>';
+                echo '<td>' . $producto['nombre'] . '</td>';
+                echo '<td>' . $producto['marca'] . '</td>';
+                echo '<td>' . $producto['modelo'] . '</td>';
+                echo '<td>' . $producto['precio'] . '</td>';
+                echo '<td>' . $producto['unidades'] . '</td>';
+                echo '<td>' . utf8_encode($producto['detalles']) . '</td>';
+                echo '<td><img src="' . $producto['imagen'] . '" alt="producto"/></td>';
+                echo '</tr>';
+            }
+
+            echo '</tbody>';
+            echo '</table>';
+            echo '</body>';
+            echo '</html>';
 
             /** útil para liberar memoria asociada a un resultado con demasiada información */
             $result->free();
-
-            if (!empty($data))
-            {
-                echo '<table class="table">';
-                echo '<thead class="thead-dark">';
-                echo '<tr>';
-                echo '<th scope="col">#</th>';
-                echo '<th scope="col">Nombre</th>';
-                echo '<th scope="col">Marca</th>';
-                echo '<th scope="col">Modelo</th>';
-                echo '<th scope="col">Precio</th>';
-                echo '<th scope="col">Unidades</th>';
-                echo '<th scope="col">Detalles</th>';
-                echo '<th scope="col">Imagen</th>';
-                echo '</tr>';
-                echo '</thead>';
-                echo '<tbody>';
-
-                foreach ($data as $row)
-                {
-                    echo '<tr>';
-                    echo '<th scope="row">' . $row['id'] . '</th>';
-                    echo '<td>' . $row['nombre'] . '</td>';
-                    echo '<td>' . $row['marca'] . '</td>';
-                    echo '<td>' . $row['modelo'] . '</td>';
-                    echo '<td>' . $row['precio'] . '</td>';
-                    echo '<td>' . $row['unidades'] . '</td>';
-                    echo '<td>' . utf8_encode($row['detalles']) . '</td>';
-                    echo '<td><img src="' . $row['imagen'] . '" ></td>';
-                    echo '</tr>';
-                }
-
-                echo '</tbody>';
-                echo '</table>';
-            }
-            else
-            {
-                echo '<p>No hay productos con unidades menores o iguales a ' . $tope . '</p>';
-            }
         }
 
         $link->close();
     }
-    ?>
-</body>
-</html>
-
+?>
